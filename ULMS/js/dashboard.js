9e5079d5c3278f3c4f64da440144e478ff127d17
SHRI-KRISHNA-S <krishnask4847@gmail.com>
2026-02-16 14:20:36 +0530

    navigation tags change

--- ULMS/js/dashboard.js
+++ ULMS/js/dashboard.js
@@ -18,19 +18,3 @@ dashboardData.forEach(item => {
 
     cardsContainer.appendChild(card);
 });
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    const currentPage = window.location.pathname.split("/").pop();
-    const menuItem = document.querySelectorAll(".sidebar-container li");
-
-    menuItem.forEach(item => {
-        if(item.dataset.page === currentPage) {
-            item.classList.add("active");
-        }
-
-        item.addEventListener("click", () => {
-            window.location.href = item.dataset.page;
-        });
-    });
-});
--- ULMS/js/layout.js
+++ ULMS/js/layout.js
@@ -15,3 +15,21 @@ document.addEventListener("DOMContentLoaded", () =>{
     });
 
 });
+
+
+// datalink
+
+document.addEventListener("DOMContentLoaded", () => {
+    const currentPage = window.location.pathname.split("/").pop();
+    const menuItem = document.querySelectorAll(".sidebar-container li");
+
+    menuItem.forEach(item => {
+        if(item.dataset.page === currentPage) {
+            item.classList.add("active");
+        }
+
+        item.addEventListener("click", () => {
+            window.location.href = item.dataset.page;
+        });
+    });
+});
